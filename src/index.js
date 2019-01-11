da2013f5983146fccbe19630f088a3f1efccdd2b
Anton Shevchenko <41957518+Karikatun@users.noreply.github.com>
2019-01-11 15:50:11 +0300

    Added tabs

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ window.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     let popupSignIn = require('./parts/popupSignIn'),
-        slider = require('./parts/slider');
+        slider = require('./parts/slider'),
+        tabs = require('./parts/tabs');
 
     popupSignIn();
     slider();
+    tabs();
 });
---
+++ src/parts/tabs.js
@@ -0,0 +1,40 @@
+function tabs() {
+    
+    let tab = document.querySelectorAll('.header-tab'),
+        menu = document.querySelector('.portfolio-menu'),
+        tabContent = document.querySelectorAll('.tabcontent');
+
+    function hideContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+        for (let i = a; i < tab.length; i++){
+            tab[i].classList.remove('active');
+        }
+    }
+    hideContent(1);
+
+    function showContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+        tab[b].classList.add('active');
+    }
+
+    menu.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target && target.classList.contains('header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideContent(0);
+                    showContent(i);
+                    break;
+                }
+            }
+        }
+    });
+}
+
+module.exports = tabs;
